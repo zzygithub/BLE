75e8e82c7d6a284f2206cdb6f94b34e6e59bdead
Ding Soung <DingSoung@gmail.com>
2015-02-16 14:22:12 +0800

    Initial Commit

---
+++ BLE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE
+//
+//  Created by 丁松 on 15/2/16.
+//  Copyright (c) 2015年 丁松. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
